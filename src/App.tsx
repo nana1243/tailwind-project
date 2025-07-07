b7c1bb52b2a6ff51ad02e7aaef4b8fb7bf94937b
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-08 07:23:57 +0900

    fea)[7/8] twMerge 적

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
+import {twMerge} from "tailwind-merge";
 
 function App() {
+  const isRed = false;
+
   return (
     <>
         <h1 className="text-3xl font-bold underline">
@@ -8,7 +11,7 @@ function App() {
         </h1>
         <div className="flex flex-col item-center justify-center-min-h-screen">
             <button className="button">Click Me! </button>
-            <button className="btns bg-rose-500">Click Me! </button>
+            <button className={twMerge(`btns`,  isRed && `bg-rose-500`)}>Click Me! </button>
         </div>
     </>
   )
