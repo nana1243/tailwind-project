7b64a50587512f237a0b5f42807cb8ab7f67ee95
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2025-07-13 11:27:47 +0900

    fea)[7/11] 폰트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import {twMerge} from "tailwind-merge";
 import fruit from './assets/images/fruit.jpg';
+import Lamp from "./components/Lamp/Lamp";
 
 
 function App() {
@@ -8,15 +9,16 @@ function App() {
 
   return (
     <>
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
+        <h1 className="michroma-regular">
+            안녕하세요! Welcome to my React App!
         </h1>
-        <div className="flex flex-col item-center justify-center-min-h-screen">
-            <button className="button">Click Me! </button>
-            <button className={twMerge(`btns`,  isRed && `bg-rose-500`)}>Click Me! </button>
-        </div>
-        <img src="/fruit.jpg"/>
-        <img src={fruit}/>
+        {/*<div className="flex flex-col item-center justify-center-min-h-screen">*/}
+        {/*    <button className="button">Click Me! </button>*/}
+        {/*    <button className={twMerge(`btns`,  isRed && `bg-rose-500`)}>Click Me! </button>*/}
+        {/*</div>*/}
+        {/*<img src="/fruit.jpg"/>*/}
+        {/*<img src={fruit}/>*/}
+        <Lamp/>
     </>
   )
 }
